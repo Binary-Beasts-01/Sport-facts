99b51a6230a31c6c211055e43bbf8e4d819c9139
Naboni Abebe <se.naboni.abebe@gmail.com>
2021-02-20 18:16:28 +0300

    About and services page added.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,48 @@
-var menuList = document.getElementById("menuList");
-
-menuList.style.maxHeight = "0px";
-
-function toggleMenu(){
-    if(menuList.style.maxHeight == "0px"){
-        menuList.style.maxHeight = "190px"
-    }else{
-        menuList.style.maxHeight = "0px"
-    }
-}
-
-
 $(document).ready(function () {
+
+
+    let $btns = $('.project-area .button-group button');
+
+
+    $btns.click(function (e) {
+
+        $('.project-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.project-area .grid').isotope({
+            filter: selector
+        });
+
+        return false;
+    })
+
+    $('.project-area .button-group #btn1').trigger('click');
+
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery: { enabled: true }
+    });
+
+
+    // Owl-carousel
+
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            560: {
+                items: 2
+            }
+        }
+    })
+
+    // sticky navigation menu
+
     let nav_offset_top = $('.header_area').height() + 50;
 
     function navbarFixed() {
@@ -28,4 +59,5 @@ $(document).ready(function () {
     }
 
     navbarFixed();
+
 });
