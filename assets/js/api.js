f3f413c66c980c6ca4227c4ae40f76fd12027fb3
Surafel Kassahun <se.surafel.kassahun@gmail.com>
2021-02-25 13:19:10 +0300

    Api fetch request exported

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,50 +1,32 @@
-function fetchTeamData(query) {
+export function fetchTeamData(query, callback) {
     fetch(`https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${query}`).then(function(response) {
     return response.json();
   }).then(function(json) {
     let products = json;
-    displayTeam(products);
+    callback(products);
   }).catch(function(err) {
     console.log('Fetch problem: ' + err.message);
   });
 }
 
-fetchTeamData("Barcelona");
-
-function displayTeam(data) {
-    console.log(data.teams);
-} 
-
-function fetchPlayerData(firstname, lastname) {
-    fetch(`https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=${firstname}%20${lastname}`).then(function(response) {
+export function fetchPlayerData(name, callback) {
+    fetch(`https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=${name}`).then(function(response) {
     return response.json();
   }).then(function(json) {
     let products = json;
-    displayPlayer(products);
+    callback(products);
   }).catch(function(err) {
     console.log('Fetch problem: ' + err.message);
   });
 }
 
-fetchPlayerData("Cristiano", "Ronaldo");
-
-function displayPlayer(data) {
-    console.log(data.player);
-} 
-
-function fetchAllTeamsInALeague(leagueName) {
+function fetchAllTeamsInALeague(leagueName, callback) {
   fetch(`https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=${leagueName}`).then(function(response) {
   return response.json();
 }).then(function(json) {
   let products = json;
-  displayAllTeamsInLeague(products);
+  callback(products);
 }).catch(function(err) {
   console.log('Fetch problem: ' + err.message);
 });
 }
-
-fetchAllTeamsInALeague("English_Premier_League");
-
-function displayAllTeamsInLeague(data) {
-  console.log(data.teams);
-} 
