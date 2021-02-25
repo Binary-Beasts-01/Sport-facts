b4ee3e06f7138ce3e767aaacc068e036e8a936a5
nabek <nabekabebe6@gmail.com>
2021-02-25 11:48:07 +0300

    Nabek: filtering with dynamic data fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,6 @@ $(document).ready(function () {
   $btns.click(function (e) {
     $('.project-area .button-group button').removeClass('active');
     e.target.classList.add('active');
-
     let selector = $(e.target).attr('data-filter');
     $('.project-area .grid').isotope({
       filter: selector,
